aa96929a8a60d00205c5710e841722a1b3b81417
okjack <okjackcaptain@gmail.com>
2024-10-05 09:56:52 +0800

    armv7

--- buildscripts/include/ci.sh
+++ buildscripts/include/ci.sh
@@ -34,7 +34,7 @@ build_prefix() {
 	# build everything mpv depends on (but not mpv itself)
 	for x in ${dep_mpv[@]}; do
 		msg "Building $x"
-		./buildall.sh $x
+		./buildall.sh --arch armv7l $x
 	done
 
 	if [[ "$CACHE_MODE" == folder && -w "$CACHE_FOLDER" ]]; then
@@ -79,13 +79,13 @@ else
 fi
 
 msg "Building mpv"
-./buildall.sh -n mpv || {
+./buildall.sh -n --arch armv7l mpv || {
 	# show logfile if configure failed
 	[ ! -f deps/mpv/_build/config.h ] && cat deps/mpv/_build/meson-logs/meson-log.txt
 	exit 1
 }
 
 msg "Building mpv-android"
-./buildall.sh -n
+./buildall.sh -n --arch armv7l
 
 exit 0
